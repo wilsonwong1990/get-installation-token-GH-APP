97462fc4431f86a2579250d1b5343619dffee649
Wilson Wong <wilsonwong1990@github.com>
2020-04-24 22:13:40 -0500

    added ruby version

--- README.md
+++ README.md
@@ -9,12 +9,14 @@ When a Github App is created, you [generate a private key](https://developer.git
 This script will take your private key, generate the JWT then output an installation token for use. The script is more or less a wrapper for the ruby script that generates the JWT and then queries the [installation endpoint](https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-a-github-app) for you with it.
 
 
-#### Requirements:
+#### Requirements for bash version:
 
 * ruby w/ openssl and jwt
 * a GitHub App private key 
 
 #### How to use: 
+
+This was my original version that wrapped was a shell script that wrapped the ruby script. 
 
 Make sure the scripts are executable and run:
 
@@ -24,4 +26,13 @@ Make sure the scripts are executable and run:
 
 Follow the prompts, and enter the path to the private key and the installation id when asked.
 
+#### Requirements for ruby version:
 
+* openssl
+* jwt
+* net/http
+* uri
+
+Just run ./get-installation-token.rb
+
+Follow the prompts and enter the path to the private key and enter the installation id when asked.
---
+++ get-installation-token.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby
+
+require 'openssl'
+require 'jwt'  # https://rubygems.org/gems/jwt
+require 'net/http'
+require 'uri'
+
+puts "Where is the GitHub App certificate? (.pem format)"
+pem_path = gets.chomp
+
+puts "What is the installation id?"
+installation_id = gets.chomp
+
+# Private key contents
+pem = File.read("#{pem_path}")
+#private_pem = File.read("pem")
+private_key = OpenSSL::PKey::RSA.new(pem)
+
+# Generate the JWT
+payload = {
+  # issued at time
+  iat: Time.now.to_i,
+  # JWT expiration time (10 minute maximum)
+  exp: Time.now.to_i + (10 * 60),
+  # GitHub App's identifier
+  iss: 61967
+}
+
+jwt = JWT.encode(payload, private_key, "RS256")
+puts jwt
+
+uri = URI.parse("https://api.github.com/app/installations/#{installation_id}/access_tokens")
+puts uri
+request = Net::HTTP::Post.new(uri)
+request["Authorization"] = "Bearer #{jwt}"
+request["Accept"] = "application/vnd.github.machine-man-preview+json"
+
+req_options = {
+  use_ssl: uri.scheme == "https",
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  http.request(request)
+end
+puts response.body
